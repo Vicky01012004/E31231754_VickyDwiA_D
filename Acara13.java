259382c41daa1f5246767ca51c36f71cc14fcf39
Vicky01012004 <vickydwiananto7@gmail.com>
2024-01-30 14:43:00 +0700

    Create Acara13.java

---
+++ Acara13.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acara.pkg13;
+
+public class Acara13 {
+    public static void main(String[] args) {
+        int b = 9;
+        System.out.println("Sebelum do");
+        do {
+            System.out.println("Nilai b : " + b);
+            b--;
+        } while (b >= 10);
+        System.out.println("Setelah do");
+    }
+}
